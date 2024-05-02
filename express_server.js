4f5ee0dcf1b928399ca28d55d99f6486d988ee20
Bushra Patel <bpatel60@yahoo.com>
2024-05-01 22:41:12 -0400

    Add a route to GET the Registration Form '/register'

--- express_server.js
+++ express_server.js
@@ -107,6 +107,11 @@ app.post('/logout', (req, res) => {
   res.redirect('/urls');
 });
 
+//Registration Route
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
