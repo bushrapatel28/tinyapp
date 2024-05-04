ff6503e93e7926704530b5fabbaa9bf93fe2811a
Bushra Patel <bpatel60@yahoo.com>
2024-05-04 01:32:30 -0400

    Modify /edit and /delete to restrict the editing and deleting of links by owners/creators only. Onlu display /urls if user is logged in and only show the links that belong to the logged in user

--- express_server.js
+++ express_server.js
@@ -14,8 +14,14 @@ app.use(cookieParser());
 
 //Database
 const urlDatabase = {
-  b2xVn2: "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "user2RandomID"
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: "userRandomID"
+  }
 };
 
 const users = {
@@ -40,13 +46,26 @@ const generateRandomString = function() {
 //User lookup helper function
 const getUserByEmail = function(formEmail) {
   for (const userId in users) {           //Each property of users object
-    const user = users[userId];           //Each user object (sub-object) of the users object
+    const user = users[userId];           //Each user object (sub-object) of the user object
     if (user.email === formEmail) {
       return user;
     }
   }
   return null;
 };
+
+//Filter urlDatabase function
+const urlsForUser = function (id) {
+  const urls = {};
+  for (const urlID in urlDatabase) {        //Each property of urlDatabase
+    const urlObj = urlDatabase[urlID];      //Each url Object of the urlID
+    if (urlObj.userID === id) {
+      urls[urlID] = urlObj;
+      console.log(urls);
+    }
+  }
+  return urls;
+}
 
 //Logged in User lookup helper function
 const getLoggedInUser = function(formEmail, formPassword) {
@@ -73,10 +92,17 @@ app.get("/hello", (req, res) => {
 
 app.get("/urls", (req, res) => {
   const userID = req.cookies["user_id"];      //Cookie header parsed data
+  
+  if (!userID) {
+    return res.status(401).end("Please Login or Register to continue.");
+  };
+
+  const userUrls = urlsForUser(userID);       //Returned Urls Object
+
   const user = users[userID];
   const templateVars = { 
     user: user,                 //Include user object into the templateVars and pass it to the ejs file
-    urls: urlDatabase
+    urls: userUrls
   };
   res.render("urls_index", templateVars);
 });
@@ -94,11 +120,23 @@ app.get("/urls/new", (req, res) => {
 //If path is /urls/b2xVn2 then req.params.id would be b2xVn2
 app.get("/urls/:id", (req, res) => {          //:id is the route parameter
   const userID = req.cookies["user_id"];      //form info that was sent to the server
+  
+  if (!userID) {
+    return res.status(401).end("Please Login or Register to continue.");
+  };
   const user = users[userID];
+  
+  const userUrls = urlsForUser(userID);       //Returned Urls Object
+  const keys = Object.keys(userUrls);
+  
+  if (!keys.includes(req.params.id)) {
+    return res.status(403).end("Error 403: Access Denied");
+  }
+  
   const templateVars = {
     user: user,
     id: req.params.id,
-    longURL: urlDatabase[req.params.id] 
+    longURL: urlDatabase[req.params.id].longURL
   };
   res.render("urls_show", templateVars);
 });
@@ -108,21 +146,27 @@ app.post("/urls", (req, res) => {
   //console.log(req.body);              //form info that was sent to the server
   const userID = req.cookies["user_id"];
   if (!userID) {
-    return res.status(403).end("Please Login to make changes.");
-  }
+    return res.status(401).end("Please Login to make changes.");
+  };
   
   const id = generateRandomString();
   const newlongURL = req.body.longURL;
   
-  urlDatabase[id] = newlongURL;         //Add the new URL to the Database
+  urlDatabase[id] = id;
+  urlDatabase[id] = {
+    longURL: newlongURL,        //Add the new URL to the Database
+    userID: userID
+  };
+
+  console.log(urlDatabase);
   
-  res.redirect(`/urls/${id}`);          //Redirect to '/urls/:id' route
+  res.redirect(`/urls/${id}`);                  //Redirect to '/urls/:id' route
 
 });
 
 //Redirect user to the longURL when they click on the shortURL link.
 app.get('/u/:id', (req, res) => {
-  const longURL = urlDatabase[req.params.id];
+  const longURL = urlDatabase[req.params.id].longURL;
   if (!longURL) {                                           //If :id does not exist in the database
     return res.status(404).end("Error 404: Page Not Found");
   }
@@ -131,17 +175,51 @@ app.get('/u/:id', (req, res) => {
 
 //Delete/Remove a URL resource
 app.post('/urls/:id/delete', (req, res) => {
+  const userID = req.cookies["user_id"];
+  if (!userID) {
+    return res.status(401).end("Please Login to make changes.");
+  };
+
   const id = req.params.id;
+  const ids = Object.keys(urlDatabase);
+  if (!ids.includes(req.params.id)) {
+    return res.status(400).end("Error 400: Not Found");
+  }
+  
+  const userUrls = urlsForUser(userID);       //Returned Urls Object
+  const keys = Object.keys(userUrls);
+  
+  if (!keys.includes(req.params.id)) {
+    return res.status(403).end("Error 403: Access Denied");
+  };
+  
   delete urlDatabase[id];
   res.redirect('/urls');
 });
 
 //Edit/Update a URL resource
 app.post('/urls/:id',(req, res) => {
+  const userID = req.cookies["user_id"];
+  if (!userID) {
+    return res.status(401).end("Please Login to make changes.");
+  };
+
   const id = req.params.id;
+  const ids = Object.keys(urlDatabase);
+  if (!ids.includes(id)) {
+    return res.status(400).end("Error 400: Not Found");
+  }
+  
+  const userUrls = urlsForUser(userID);       //Returned Urls Object
+  const keys = Object.keys(userUrls);
+  
+  if (!keys.includes(id)) {
+    return res.status(403).end("Error 403: Access Denied");
+  };
+
   const modifiedURL = req.body.longURL;
 
-  urlDatabase[id] = modifiedURL;          //Updating urlDatabase with modified longURL
+  urlDatabase[id].longURL = modifiedURL;          //Updating urlDatabase with modified longURL
   res.redirect('/urls');
 });
 
